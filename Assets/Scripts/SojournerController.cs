921620ea9f9d5c6e042b323e4c7a3056621e2549
Kevin Heritage <kevin.j.heritage@gmail.com>
2021-09-28 21:56:34 -0400

    1st Commit

---
+++ Assets/Scripts/SojournerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SojournerController : MonoBehaviour
+{
+    private Rigidbody sojournerRigidBody;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sojournerRigidBody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            sojournerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
