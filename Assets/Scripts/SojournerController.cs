8bb24aae8697cfe0f5a807eb7d7a46c51339d0a8
Kevin Heritage <kevin.j.heritage@gmail.com>
2021-10-12 14:29:38 -0400

    Made changes to sojourner controller. now using rigid body

--- Assets/Scripts/SojournerController.cs
+++ Assets/Scripts/SojournerController.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class SojournerController : MonoBehaviour
 {
-    public CharacterController sojournerController;
     public Transform sojournerCamera;
+    private Rigidbody sojournerRigidBody;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public float verticalInput;
+    public float horizontalInput;
+    private bool isUiVisible = true;
+    private bool isReceptUIVis = false;
+    private Receptacle receptacle;
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    private Inventory inventory;
+    [SerializeField] private UI_Inventory uiInventory;
 
-    //private float gravityModifier = 9.81f;
-    public float gravityModifier = 9.81f;
-    public float jumpForce = 3.0f;
-    private float directionY;
     public float sojournerWalkSpeed = 15.0f;
     public float sojournerSprintSpeed = 25.0f;
     private float sojournerSpeed;
@@ -21,15 +25,28 @@ public class SojournerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      
+        inventory = new Inventory(UseItem, 8);
+        uiInventory.SetSojournerController(this);
+        uiInventory.SetInventory(inventory);
+
+        //  Test adding pickup objects to scene
+        ItemWorld.SpawnItemWorld(new Vector3(0, 1, 3), new Item { itemType = Item.ItemType.Cube, amount = 1 });
+        ItemWorld.SpawnItemWorld(new Vector3(3, 1, 0), new Item { itemType = Item.ItemType.Cube, amount = 1 });
+        ItemWorld.SpawnItemWorld(new Vector3(0, 1, -3), new Item { itemType = Item.ItemType.Cube, amount = 1 });
+        sojournerRigidBody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
+        receptacle = GameObject.Find("Receptacle").GetComponent<Receptacle>();
+        Debug.Log(receptacle);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //----------------------------------------------------------Player Input----------------------------------------------------//
         // sprint speed
-        if (sojournerController.isGrounded && Input.GetKey(KeyCode.LeftShift)){
+        if (Input.GetKey(KeyCode.LeftShift) && isOnGround)
+        {
             sojournerSpeed = sojournerSprintSpeed;
         }
         else
@@ -38,182 +55,97 @@ public class SojournerController : MonoBehaviour
         }
 
         // movement
-        float verticalInput = Input.GetAxis("Vertical");
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput).normalized; // normalized prevents player from moving faster diagonally
-
-        if (direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + sojournerCamera.eulerAngles.y;
-            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); // smooth turn angle
-            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
-
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; // rotation to direction, to move player into direction of camera
-            sojournerController.Move(moveDirection.normalized * sojournerSpeed * Time.deltaTime);
-        }
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Move Forward or backwards when w or s key is pressed
+        transform.Translate(Vector3.forward * Time.deltaTime * sojournerSpeed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * sojournerSpeed * horizontalInput);
 
         // jump
-        if (Input.GetKey(KeyCode.Space) && sojournerController.isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-
-            directionY = jumpForce;
+            sojournerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+        //----------------------------------------------------------Player Inventory----------------------------------------------------//
+        //  Toggle Player Inventory
+        if (Input.GetKeyDown(KeyCode.Tab) && isUiVisible == true)
+        {
+            HideUI();
+            SetIsUiVisible(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && isUiVisible == false)
+        {
+            ShowUI();
+            SetIsUiVisible(true);
         }
 
-        directionY -= gravityModifier * Time.deltaTime;
-        direction.y = directionY;
-        sojournerController.Move(direction * Time.deltaTime);
+        // Toggle Receptacle UI with E
+        if (Input.GetKeyDown(KeyCode.E) && receptacle.GetIsPlayerColliding() == true)
+        {
+            if (isReceptUIVis == false)
+            {
+                receptacle.Interact();
+                isReceptUIVis = true;
+                if (GetIsUiVisible() == false)
+                {
+                    ShowUI();
+                    SetIsUiVisible(true);
+                }
+            }
+            else
+            {
+                receptacle.HideUI();
+                isReceptUIVis = false;
+                HideUI();
+                SetIsUiVisible(false);
+            }
+        }
 
     }
 
-    
-
-    // lock cursor to middle of screen if game is focused
-    private void OnApplicationFocus(bool focus)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (focus)
+        isOnGround = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ItemWorld itemWorld = other.GetComponent<ItemWorld>();
+        if (itemWorld != null)
         {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.None;
+            inventory.AddItemMergeAmount(itemWorld.GetItem());
+            itemWorld.DestroySelf();
         }
     }
-    //private Rigidbody sojournerRigidBody;
-    //public float jumpForce;
-    //public float gravityModifier;
-    //public bool isOnGround = true;
-    //public float speed = 20.0f;
-    //private float forwardInput;
-    //private float horizontalInput;
-    //private bool isUiVisible = true;
-    //private bool isReceptUIVis = false;
-    //private Receptacle receptacle;
 
-    //private Inventory inventory;
-    //[SerializeField] private UI_Inventory uiInventory;
-    //private void Awake()
-    //{
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
 
-    //}
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    inventory = new Inventory(UseItem, 8);
-    //    uiInventory.SetSojournerController(this);
-    //    uiInventory.SetInventory(inventory);
+    public void UseItem(Item inventoryItem)
+    {
+        Debug.Log("Use Item: " + inventoryItem);
+    }
 
-    //    //  Test adding pickup objects to scene
-    //    ItemWorld.SpawnItemWorld(new Vector3(0, 1, 3), new Item { itemType = Item.ItemType.Cube, amount = 1 });
-    //    ItemWorld.SpawnItemWorld(new Vector3(3, 1, 0), new Item { itemType = Item.ItemType.Cube, amount = 1 });
-    //    ItemWorld.SpawnItemWorld(new Vector3(0, 1, -3), new Item { itemType = Item.ItemType.Cube, amount = 1 });
+    public void SetIsUiVisible(bool isUiVisible)
+    {
+        this.isUiVisible = isUiVisible;
+    }
+    public bool GetIsUiVisible()
+    {
+        return isUiVisible;
+    }
 
-    //    sojournerRigidBody = GetComponent<Rigidbody>();
-    //    Physics.gravity *= gravityModifier;
-
-    //    receptacle = GameObject.Find("Receptacle").GetComponent<Receptacle>();
-    //    Debug.Log(receptacle);
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    /*-------------------------------Player input starts here-------------------------*/
-
-    //    if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
-    //    {
-    //        sojournerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-    //        isOnGround = false;
-    //    }
-
-    //    forwardInput = Input.GetAxis("Vertical");
-    //    horizontalInput = Input.GetAxis("Horizontal");
-
-    //    // Move Forward or backwards when w or s key is pressed
-    //    transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-    //    transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-
-    //    //  Toggle Player Inventory
-    //    if (Input.GetKeyDown(KeyCode.Tab) && isUiVisible == true)
-    //    {
-    //        HideUI();
-    //        SetIsUiVisible(false);
-    //    } else if (Input.GetKeyDown(KeyCode.Tab) && isUiVisible == false)
-    //    {
-    //        ShowUI();
-    //        SetIsUiVisible(true);
-    //    }
-
-    //    // Toggle Receptacle UI with E
-    //    if (Input.GetKeyDown(KeyCode.E) && receptacle.GetIsPlayerColliding() == true)
-    //    {
-    //        if (isReceptUIVis == false)
-    //        {
-    //            receptacle.Interact();
-    //            isReceptUIVis = true;
-    //            if (GetIsUiVisible() == false)
-    //            {
-    //                ShowUI();
-    //                SetIsUiVisible(true);
-    //            }
-    //        } else
-    //        {
-    //            receptacle.HideUI();
-    //            isReceptUIVis = false;
-    //            HideUI();
-    //            SetIsUiVisible(false);
-    //        }
-
-    //    }
-
-
-    //    /*-------------------------------Player input ends here---------------------------*/
-    //}
-
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    isOnGround = true;
-    //}
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-
-        
-    //    ItemWorld itemWorld = other.GetComponent<ItemWorld>();
-    //    if (itemWorld != null)
-    //    {
-    //        inventory.AddItemMergeAmount(itemWorld.GetItem());
-    //        itemWorld.DestroySelf();
-    //    }
-    //}
-
-    //public Vector3 GetPosition()
-    //{
-    //    return transform.position;
-    //}
-
-    //public void UseItem(Item inventoryItem)
-    //{
-    //    Debug.Log("Use Item: " + inventoryItem);
-    //}
-
-    //public void SetIsUiVisible(bool isUiVisible)
-    //{
-    //    this.isUiVisible = isUiVisible;
-    //}
-    //public bool GetIsUiVisible()
-    //{
-    //    return isUiVisible;
-    //}
-
-    //public void ShowUI()
-    //{
-    //    uiInventory.GetCanvasGroup().alpha = 1f;
-    //    uiInventory.GetCanvasGroup().blocksRaycasts = true;
-    //}
-    //public void HideUI()
-    //{
-    //    uiInventory.GetCanvasGroup().alpha = 0f;
-    //    uiInventory.GetCanvasGroup().blocksRaycasts = false;
-    //}
+    public void ShowUI()
+    {
+        uiInventory.GetCanvasGroup().alpha = 1f;
+        uiInventory.GetCanvasGroup().blocksRaycasts = true;
+    }
+    public void HideUI()
+    {
+        uiInventory.GetCanvasGroup().alpha = 0f;
+        uiInventory.GetCanvasGroup().blocksRaycasts = false;
+    }
 }
