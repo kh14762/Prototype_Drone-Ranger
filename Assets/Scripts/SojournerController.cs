e32ba322f02d5d84c663c07b46726792a2395398
JPatel1622 <pateljeel1622@yahoo.com>
2021-11-10 10:36:56 -0500

    Updated enemy movement

--- Assets/Scripts/SojournerController.cs
+++ Assets/Scripts/SojournerController.cs
@@ -67,8 +67,9 @@ public class SojournerController : MonoBehaviour
         enableInput = true;
 
         // Lock cursor when playing
-        Cursor.lockState = CursorLockMode.Locked;
         Cursor.lockState = CursorLockMode.Confined; // keep confined in the game window
+        Cursor.lockState = CursorLockMode.Locked;
+        
 
         //  Set ui_Printer
         //ui_Printer = GameObject.Find("UI_Printer").GetComponent<UI_Printer>();
