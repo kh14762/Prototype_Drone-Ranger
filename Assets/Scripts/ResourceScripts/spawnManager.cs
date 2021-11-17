66085cf6afb8883b58858d9e7c8a60363587ffa1
JustPassingAlong <81654189+JustPassingAlong@users.noreply.github.com>
2021-11-16 20:40:41 -0500

    Spawn Manager
    Spwan Manager for resource node

--- Assets/Scripts/ResourceScripts/spawnManager.cs
+++ Assets/Scripts/ResourceScripts/spawnManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class spawnManager : MonoBehaviour
 {
+
+    public GameObject nodePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject.Instantiate(nodePrefab, transform.position, transform.rotation);
     }
 
     // Update is called once per frame
@@ -15,4 +18,6 @@ public class spawnManager : MonoBehaviour
     {
         
     }
+
+    private void 
 }
