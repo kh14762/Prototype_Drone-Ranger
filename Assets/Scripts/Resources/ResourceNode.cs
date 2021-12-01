5927ef13575bc3d782005ce6524b245fe67c0f55
Kevin Heritage <kevin.j.heritage@gmail.com>
2021-11-30 20:10:57 -0500

    Updated Resource Node script

--- Assets/Scripts/Resources/ResourceNode.cs
+++ Assets/Scripts/Resources/ResourceNode.cs
@@ -4,35 +4,46 @@ using UnityEngine;
 public class ResourceNode : MonoBehaviour
 {
     private GameObject player;
-    public Item[] rawMaterials;
+    private Inventory playerInventory;
+    private bool isCoroutineRunning;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Sojourner");
-
+        playerInventory = player.GetComponent<Inventory>();
+        isCoroutineRunning = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) <= 5)
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                if (isCoroutineRunning)
+                {
+                    Debug.Log("Starting Coroutine");
+                    StartCoroutine(Countdown());
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.F))
+            {
 
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == player && Input.GetKeyDown(KeyCode.F))
-        {
-            while (true)
-            {
-                StartCoroutine(Countdown());
-            }
-        }
+       
     }
 
     // Raw material corountine 
     IEnumerator Countdown()
     {
+        //Add materials to player inventory here
+        playerInventory.AddItem(new Item { itemType = Item.ItemType.MetalScrap, amount = 1 });
         yield return new WaitForSeconds(3);
-
     }
 }
